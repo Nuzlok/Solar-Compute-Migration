a1c08469e93b7b38c685f2727b37f91ef12b930d
Saksham Goyal <sakgoy2001@vt.edu>
2023-03-21 14:50:48 -0400

    remove unused files

--- test_code/restorer.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-os.system(f"unshare -p -m --fork --mount-proc")
-os.system(f"ip addr add 192.168.137.2/24 dev eth0")
-
-os.system(f"cd {sys.argv[1]}")
-os.system(f"criu restore -vvvv -o restore.log --shell-job --tcp-established")
